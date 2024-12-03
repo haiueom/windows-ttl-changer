61b9656547c7f0fadbd0467472d7038ad27a1035
Ilham Taufiq <haiueom@gmail.com>
2024-12-03 20:09:10 +0800

    update by me

--- Readme.md
+++ Readme.md
@@ -1,15 +1,20 @@
-# TTL Changer
-**Download Here**: [![](https://img.shields.io/github/downloads/AzimsTech/TTL-Changer/1.4/TTL-Changer_1.4.bat.svg)](https://github.com/AzimsTech/TTL-Changer/releases/download/1.4/TTL-Changer_1.4.bat)
+# Windows TTL Changer
 
-## What is TTL?
+<!-- **Download Here**: [![](https://img.shields.io/github/downloads/AzimsTech/TTL-Changer/1.4/TTL-Changer_1.4.bat.svg)](https://github.com/AzimsTech/TTL-Changer/releases/download/1.4/TTL-Changer_1.4.bat) -->
 
-Time to Live (TTL) represents to number of 'hops' a packet can take before it is considered invalid. For Windows/Windows Phone, this value is 128. This value is 64 for Linux/Android.
+## Overview
+
+Time to Live (TTL) is a crucial parameter in networking that defines the number of hops a packet can take before it is discarded.
+
+- Windows: Default TTL = 128
+- Linux/Android: Default TTL = 64
 
 ![](https://i.imgur.com/yhhO5mP.png)
 
-## Why Change this value at all?
+## Why Change TTL?
 
-Some ISP prohibit 'tethering' your phone to another device. By changing TTL value to 60, the carriers must think you're using the phone, not the device it's tethered to.
+Some Internet Service Providers (ISPs) may restrict tethering by detecting whether a device other than your phone is connected to the network. By setting the TTL value to 65, the traffic appears as though it originates directly from your phone, bypassing such restrictions.
 
 # Research
-- [Possible ways of detecting hotspot](https://android.stackexchange.com/questions/47819/how-can-phone-companies-detect-tethering-incl-wifi-hotspot)
+
+- [How ISPs detect tethering/hotspot](https://android.stackexchange.com/questions/47819/how-can-phone-companies-detect-tethering-incl-wifi-hotspot)
--- TTL-Changer.bat
+++ TTL-Changer.bat
@@ -1,73 +1,90 @@
 @echo off
-title TTL Changer 1.4 - AzimsTech
-color 1f
+title Windows TTL Changer - haiueom
+color 0a
 
-:: Check if it running as administator. If not, then prompt an administator request
+:: Check if running as administrator
 if not "%1"=="am_admin" (
-    TIMEOUT 2 > NUL
-    @ECHO :: Requesting administator access...
-    powershell -Command "Start-Process -Verb RunAs -FilePath '%0' -ArgumentList 'am_admin'"
-    exit /b
+  echo [!] Not running as administrator
+  echo [*] Requesting administrator access...
+  timeout 2 > NUL
+  powershell -Command "Start-Process -Verb RunAs -FilePath '%~f0' -ArgumentList 'am_admin'"
+  goto EOF
 )
-
 
 :MENU
 	CLS
 	:: Get current TTL value from localhost
 	for /f "tokens=6" %%i in ('ping -n 1 127.0.0.1^|find "TTL"') do set ttl="%%i"
 
-	:: Change background color to red if TTL=128 else, green.
-	if %ttl% == "TTL=128" ( color 4F ) else ( color A0 )
-
-	
-:::   _____ _____ _        ___ _  _   _   _  _  ___ ___ ___ 
-:::  |_   _|_   _| |  ___ / __| || | /_\ | \| |/ __| __| _ \
-:::    | |   | | | |_|___| (__| __ |/ _ \| .` | (_ | _||   /
-:::    |_|   |_| |____|   \___|_||_/_/ \_\_|\_|\___|___|_|_\
-	:: Display ASCII art
-	for /f "delims=: tokens=*" %%A in ('findstr /b ::: "%~f0"') do @echo(%%A
-                                                        
-
 	:: Display current TTL value
 	echo --------------------------------------------------------------
-	echo  Current %ttl%
+	echo Current TTL: %ttl%
 	echo --------------------------------------------------------------
 	echo.
-	echo [1] 60 (bypass)
+	echo [1] 65 (bypass)
 	echo [2] 128 (default)
 	echo [3] Exit
 	echo.
 
 	:: Get user input
-	CHOICE /C:123
-	echo.
+	CHOICE /C 123 /N /M "[?] Choose an option: "
+  echo.
 
-	if errorlevel 1 set M=1
-	if errorlevel 2 set M=2
-	if errorlevel 3 set M=3
-	if %M%==1 goto BYPASS
-	if %M%==2 goto DEFAULT
-	if %M%==3 goto EOF
- exit /b
+	:: Process the choice
+  if errorlevel 3 set M=3
+  if errorlevel 2 set M=2
+  if errorlevel 1 set M=1
+
+  :: Redirect based on user input
+  if "%M%"=="1" goto BYPASS
+  if "%M%"=="2" goto DEFAULT
+  if "%M%"=="3" goto EOF
+
+  :: Default exit in case of unexpected input
+  echo [!] Invalid choice. Exiting...
+  goto EOF
 
 :: TTL set to 65 (bypass) & back to menu
 :BYPASS
-	netsh int ipv4 set glob defaultcurhoplimit=60 >NUL
-	netsh int ipv6 set glob defaultcurhoplimit=60 >NUL
-	:: prefer ipv4 over ipv6 https://docs.microsoft.com/en-us/troubleshoot/windows-server/networking/configure-ipv6-in-windows
-	reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\Tcpip6\Parameters" /v DisabledComponents /t REG_DWORD /d 32 /f
-	echo.
-	echo Restart now for best results.
-	timeout 2 >NUL
-	goto MENU
+  :: Set TTL to 65 for IPv4 and IPv6
+  echo [*] Setting TTL to 65 (bypass)...
+  netsh int ipv4 set glob defaultcurhoplimit=65 > NUL
+  netsh int ipv6 set glob defaultcurhoplimit=65 > NUL
+
+  :: Prefer IPv4 over IPv6
+  :: Reference: https://docs.microsoft.com/en-us/troubleshoot/windows-server/networking/configure-ipv6-in-windows
+  echo [*] Configuring preference for IPv4 over IPv6...
+  reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\Tcpip6\Parameters" /v DisabledComponents /t REG_DWORD /d 32 /f > NUL
+
+  :: Inform user about the change
+  echo.
+  echo [✓] TTL set to 65 (bypass) successfully.
+  echo [!] Restart now for best results.
+  timeout 5 > NUL
+
+  :: Return to the main menu
+  goto MENU
 
 :: TTL set to 128 (default value) & back to menu
 :DEFAULT
-	netsh int ipv4 set glob defaultcurhoplimit=128 >NUL
-	netsh int ipv6 set glob defaultcurhoplimit=128 >NUL
-	:: revert prefer ipv4 over ipv6 https://docs.microsoft.com/en-us/troubleshoot/windows-server/networking/configure-ipv6-in-windows
-	reg delete "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\Tcpip6\Parameters" /v DisabledComponents /f
-	echo.
-	echo Restart now for best results.
-	timeout 2 >NUL
-	goto MENU
+  :: Set TTL to 128 for IPv4 and IPv6
+  echo [*] Setting TTL to 128 (default value)...
+  netsh int ipv4 set glob defaultcurhoplimit=128 > NUL
+  netsh int ipv6 set glob defaultcurhoplimit=128 > NUL
+
+  :: Revert preference for IPv4 over IPv6
+  :: Reference: https://docs.microsoft.com/en-us/troubleshoot/windows-server/networking/configure-ipv6-in-windows
+  echo [*] Reverting preference for IPv4 over IPv6...
+  reg delete "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\Tcpip6\Parameters" /v DisabledComponents /f > NUL
+
+  :: Inform user about the change
+  echo.
+  echo [✓] TTL set to 128 (default value) successfully.
+  echo [!] Restart now for best results.
+  timeout 5 > NUL
+
+  :: Return to the main menu
+  goto MENU
+
+:EOF
+  exit /b
